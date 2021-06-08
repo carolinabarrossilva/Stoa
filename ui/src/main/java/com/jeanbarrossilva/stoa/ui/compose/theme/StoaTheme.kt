7e6e00e200678c22f25db9cafbe99b5b933fb229
Jean Silva <jeanbarrossilva@outlook.com>
2021-06-08 19:14:25 -0300

    Add Surface Composable to default StoaTheme implementation

--- ui/src/main/java/com/jeanbarrossilva/stoa/ui/compose/theme/StoaTheme.kt
+++ ui/src/main/java/com/jeanbarrossilva/stoa/ui/compose/theme/StoaTheme.kt
@@ -6,26 +6,25 @@ import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.jeanbarrossilva.stoa.extensions.compose.fontfamily.fontFamilyResource
+import com.jeanbarrossilva.stoa.extensions.compose.modifier.doIf
 import com.jeanbarrossilva.stoa.extensions.compose.theme.AndroidTheme
 import com.jeanbarrossilva.stoa.ui.R
 
 @Composable
 private fun StoaTheme(modifier: Modifier = Modifier, fills: Boolean, content: @Composable () -> Unit) {
     AndroidTheme(Typography(fontFamilyResource(R.font.android_euclid_medium))) {
-        if (fills)
-            Surface(
-                modifier
-                    .fillMaxSize(),
-                content = content
-            )
-        else
-            content()
+        Surface(
+            modifier.doIf(fills) {
+                fillMaxSize()
+            },
+            content = content
+        )
     }
 }
 
 @Composable
 fun StoaTheme(modifier: Modifier, content: @Composable () -> Unit) {
-    StoaTheme(fills = true, content = content)
+    StoaTheme(modifier, fills = true, content = content)
 }
 
 @Composable
