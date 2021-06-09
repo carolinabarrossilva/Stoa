ee45e2b7b07daf35e8a52fd8201183b920e30f9a
Jean Silva <jeanbarrossilva@outlook.com>
2021-06-09 18:12:23 -0300

    Fix Any.doIf implementation in :extensions-compose

--- extensions-compose/src/main/java/com/jeanbarrossilva/stoa/extensions/compose/any/Any.kt
+++ extensions-compose/src/main/java/com/jeanbarrossilva/stoa/extensions/compose/any/Any.kt
@@ -4,5 +4,5 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun <T> T.doIf(condition: Boolean, action: @Composable T.() -> T): T {
-    return if (condition) this else action()
+    return if (condition) action() else this
 }
