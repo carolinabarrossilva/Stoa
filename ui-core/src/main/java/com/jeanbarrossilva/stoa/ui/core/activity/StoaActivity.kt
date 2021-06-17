32db0da6d45eb0c388c6b55bf5d5f763e1370064
Jean Silva <jeanbarrossilva@outlook.com>
2021-06-17 08:10:28 -0300

    Add and implement :ui-core's StoaActivity

---
+++ ui-core/src/main/java/com/jeanbarrossilva/stoa/ui/core/activity/StoaActivity.kt
@@ -0,0 +1,41 @@
+package com.jeanbarrossilva.stoa.ui.core.activity
+
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.jeanbarrossilva.stoa.extensions.context.activity.view
+import com.jeanbarrossilva.stoa.extensions.view.drawerlayout.toggle
+import com.jeanbarrossilva.stoa.extensions.view.searchFor
+import com.jeanbarrossilva.stoa.ui.core.R
+
+open class StoaActivity: AppCompatActivity() {
+    private fun configNavigation() {
+        val toolbar = view?.searchFor<Toolbar>()
+        val drawerLayout = view?.searchFor<DrawerLayout>()
+
+        if (toolbar != null && drawerLayout != null) {
+            setSupportActionBar(toolbar)
+            setupActionBarWithNavController(findNavController(), drawerLayout)
+            toolbar.setNavigationOnClickListener {
+                drawerLayout.toggle()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        configNavigation()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == android.R.id.home) findNavController().popBackStack() else super.onOptionsItemSelected(item)
+    }
+
+    fun findNavController(): NavController {
+        return findNavController(R.id.container)
+    }
+}
