7295aa7fea0755d742c54690461616e3498a4b64
Jean Silva <jeanbarrossilva@outlook.com>
2021-06-18 17:28:25 -0300

    Remove unused AppCompatActivity, FragmentTransaction and KClass extensions
    Closes #13

--- extensions/src/main/java/com/jeanbarrossilva/stoa/extensions/context/activity/appcompatactivity/AppCompatActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.jeanbarrossilva.stoa.extensions.context.activity.appcompatactivity
-
-import androidx.appcompat.app.AppCompatActivity
-
-val AppCompatActivity.previousFragment
-    get() = with(supportFragmentManager) {
-        primaryNavigationFragment?.parentFragmentManager?.fragments?.lastOrNull() ?: fragments.elementAtOrNull(fragments.lastIndex - 1)
-    }
