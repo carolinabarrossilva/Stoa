60505ab0515f0c15724a04b622668dd68e26bcc9
Jean Silva <jeanbarrossilva@outlook.com>
2021-06-18 17:23:18 -0300

    Remove curious 'imgeview' package in :extensions
    Closes #14

--- extensions/src/main/java/com/jeanbarrossilva/stoa/extensions/view/imgeview/ImageView.kt
+++
@@ -1,9 +0,0 @@
-package com.jeanbarrossilva.stoa.extensions.view.imgeview
-
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-
-fun ImageView.load(url: String) {
-    if (!isInEditMode)
-        Glide.with(this).load(url).into(this)
-}
--- ui/src/main/java/com/jeanbarrossilva/stoa/ui/viewholder/BookViewHolder.kt
+++ ui/src/main/java/com/jeanbarrossilva/stoa/ui/viewholder/BookViewHolder.kt
@@ -4,7 +4,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.jeanbarrossilva.stoa.extensions.view.imgeview.load
+import com.jeanbarrossilva.stoa.extensions.view.imageview.load
 import com.jeanbarrossilva.stoa.model.Book
 import com.jeanbarrossilva.stoa.ui.R
 
